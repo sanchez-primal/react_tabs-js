ebccf057db07556627ec7d4eaf72de56a861f25a
Alex Kh. <sanchez.primal@gmail.com>
2026-04-06 13:57:40 +0200

    fix tab title display in section title

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ export const App = () => {
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is {tabs.find(tab => tab.id === activeTabId).title}
+        Selected tab is{' '}
+        {tabs.find(tab => tab.id === activeTabId)?.title || tabs[0].title}
         {/*
           I'd like to pass the title here asynchronously when it's
           found in the loop that goes through tabs, e.g. with state, but
